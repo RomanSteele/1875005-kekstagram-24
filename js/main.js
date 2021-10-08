8380fb3250ee7cd50c202feb87181348b65d1951
RomanSteele <romanzelenenko@yandex.ru>
2021-10-08 12:53:55 +0300

    Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 //Функция, возвращающая случайное целое число из переданного диапазона включительно.
 
-let randomNumber;
 function getRandomNumber(min , max) {
+  let randomNumber;
   if(max > min & Math.sign(max)>=0 & Math.sign(min)>=0){
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -12,17 +12,10 @@ function getRandomNumber(min , max) {
 }
 
 getRandomNumber(0,100);
-console.log(getRandomNumber(0,100));
-
 
 //Функция для проверки максимальной длины строки.
 
-function getCheckLength (checkedLength, maxLength)
-{
-  if(checkedLength >= maxLength){
-    return false;
-  }
-  return true;
+function getCheckLength (checkMaxLength, maxLength){
+  return !(checkMaxLength.length >= maxLength);
 }
 getCheckLength(0,100);
-console.log(getCheckLength (0,100));
