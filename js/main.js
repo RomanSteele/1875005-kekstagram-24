597d6a359c565697433cf94a79b907ceccc41d0f
RomanSteele <romanzelenenko@yandex.ru>
2021-10-14 15:25:24 +0300

    Частично исправлен

--- js/main.js
+++ js/main.js
@@ -24,9 +24,10 @@ getCheckLength(0,100);
 
 const eachArray = (currentLength)  => {
   const array = new Array(currentLength);
-  return Math.floor(Math.random() * (array.length))};
+  return Math.floor(Math.random() * (array.length));
+};
 
-const descriptionArray = [
+const DESCRIPTIONS = [
   'Приятно, граждане, наблюдать, как стремящиеся вытеснить традиционное производство, нанотехнологии, которые представляют собой яркий пример континентально-европейского типа политической культуры, будут заблокированы в рамках своих собственных рациональных ограничений.',
   'Кстати, действия представителей оппозиции, которые представляют собой яркий пример континентально-европейского типа политической культуры, будут призваны к ответу.',
   'Прежде всего, понимание сути ресурсосберегающих технологий, а также свежий взгляд на привычные вещи - безусловно открывает новые горизонты для позиций, занимаемых участниками в отношении поставленных задач.',
@@ -35,7 +36,7 @@ const descriptionArray = [
   'Равным образом, внедрение современных методик однозначно фиксирует необходимость системы массового участия.',
 ];
 
-const messageArray = [
+const MESSAGES = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -44,7 +45,7 @@ const messageArray = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
 ];
 
-const nameArray = [
+const NAMES = [
   'Егор',
   'Света',
   'Артем',
@@ -53,29 +54,27 @@ const nameArray = [
   'Екатерина',
 ];
 
-const randomName = Math.floor(Math.random() * nameArray.length);
-const randomMessage = Math.floor(Math.random() * messageArray.length);
-const randomDescription = Math.floor(Math.random() * descriptionArray.length);
+const POSTS_COUNT = 25;
 
-const commentArray = () => {
-return{
-    id: eachArray(250)+1,
-    avatar: 'img/' + (eachArray(6)+1) + '.svg',
-    message: messageArray[randomMessage],
-    name: nameArray[randomName],
-}};
+const randomName = Math.floor(Math.random() * NAMES.length);
+const randomMessage = Math.floor(Math.random() * MESSAGES.length);
+const randomDescription = Math.floor(Math.random() * DESCRIPTIONS.length);
 
-const createPost = () => {
-return{
-id: eachArray(24)+1,
-url: 'photos/' + (eachArray(6)+1) + '.jpg',
-description: descriptionArray[randomDescription],
-likes: eachArray(185)+15,
-comments: commentArray(),
-}};
+const commentArray = () => ({
+  id: eachArray(250)+1,
+  avatar: `img/${  eachArray(6)+1  }.svg`,
+  message: MESSAGES[randomMessage],
+  name: NAMES[randomName],
+});
 
+const createPost = () => ({
+  id: eachArray(24)+1,
+  url: `photos/${  eachArray(6)+1  }.jpg`,
+  description: DESCRIPTIONS[randomDescription],
+  likes: eachArray(185)+15,
+  comments: commentArray(),
+});
 
-const similarPost = Array.from({length: 25}, createPost);
+const posts = Array.from({length: POSTS_COUNT}, createPost);
 
-similarPost;
-
+posts;
