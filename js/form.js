e1ba6a307ee80a643c0091d2378f74ac4aa3d0b2
RomanSteele <romanzelenenko@yandex.ru>
2021-11-16 00:40:03 +0300

    8-1

--- js/form.js
+++ js/form.js
@@ -1,8 +1,10 @@
+const form = document.querySelector('#upload-select-image');
 const shutDownFormButton = document.querySelector('#upload-cancel');
 const formElement = document.querySelector('#upload-file');
 const hashTagChecker = /^#[A-Za-zА-Я-а-яЁё0-9]{1,19}$/;
 const hashTagInput = document.querySelector('.text__hashtags');
 const descriptionInput = document.querySelector('.text__description');
+const HASH_TAGS_MAX_COUNT = 5;
 
 function showFormAdder (){
   document.querySelector('.img-upload__overlay').classList.remove('hidden');
@@ -12,6 +14,7 @@ function showFormAdder (){
 function hideFormAdder(){
   document.querySelector('.img-upload__overlay').classList.add('hidden');
   document.querySelector('body').classList.remove('modal-open');
+  form.reset();
 }
 
 shutDownFormButton.addEventListener('click', ()=>{
@@ -19,23 +22,44 @@ shutDownFormButton.addEventListener('click', ()=>{
 });
 
 document.addEventListener('keydown', (evt) =>{
-  if(evt.key === 'Escape' && hashTagChecker.onblur){
+  if(evt.key === 'Escape'){
     hideFormAdder();
   }
 });
 
-document.querySelector('#upload-file').onElementChange = formElement.addEventListener('change', () => {
+
+formElement.addEventListener('change', () => {
   showFormAdder();
 });
 
-hashTagInput.addEventListener('change', () => {
-  const hashTagArray = hashTagInput.split(' ');
-  if(hashTagArray.length <= 5){
-    if(hashTagArray.some((hashtag)=> {hashTagChecker.test(hashtag) === true;}) === true) {
-      return
-    }
-  } else {
-    hashTagInput.setCustomValidity('Not Valid!');
-  }
+descriptionInput.addEventListener('keydown', (evt) => {
+  evt.stopPropagation();
 });
+
+
+hashTagInput.addEventListener('keydown', (evt) => {
+  evt.stopPropagation();
+
+  const hashtagValue = String(evt.target.value.toLowerCase().trim());
+  const hashTags = hashtagValue.split(' ');
+  if(hashTags.length > HASH_TAGS_MAX_COUNT){
+    // eslint-disable-next-line no-template-curly-in-string
+    hashTagInput.setCustomValidity('Max hashtags count equal ${HASH_TAGS_MAX_COUNT}');
+    return;
+  }
+
+  for (let i = 0; i < hashTags.length; i++){
+    const hashTag = hashTags[i];
+    const isValid = hashTagChecker.test(hashTag);
+
+    if(!isValid){
+      hashTagInput.setCustomValidity('Hashtag is not valid!');
+      break;
+    } else{
+      hashTagInput.setCustomValidity('');
+    }
+  }
+
+});
+
 
